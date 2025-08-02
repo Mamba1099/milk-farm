d39d50d819b97410aa85eac833bfe9bd35e81bc0
Mamba1099 <mambakaranja240@gmail.com>
2025-08-02 14:37:50 +0300

    refactor(hooks): clean up index.ts by removing commented sections and unused type exports

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,15 +1,11 @@
-// Auth mutations
 export { useRegisterMutation } from "./use-register";
 export { useLoginMutation } from "./use-login";
-
-// User queries and auth mutations
 export {
   useCurrentUser,
   useFarmManagerExists,
   useLogoutMutation,
 } from "./use-user-queries";
 
-// Dashboard hooks
 export {
   useDashboardStats,
   useAnimalStats,
@@ -17,22 +13,9 @@ export {
   useTreatmentStats,
   useUserStats,
   useSystemHealth,
-  type Animal,
-  type Production,
-  type Treatment,
-  type UserStats,
-  type SystemHealth,
-  type DashboardStats,
 } from "./use-dashboard-hooks";
 
-// UI hooks
 export { useToast, type Toast } from "./use-toast";
-
-// Animal hooks
 export * from "./use-animal-hooks";
-
-// Employee hooks
 export * from "./use-employee-hooks";
-
-// Reports hooks
 export * from "./use-reports-hooks";
