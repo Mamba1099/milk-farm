3bb3bc6aa9d1d73f1df295a14969160169f260a8
Mamba1099 <mambakaranja240@gmail.com>
2025-08-04 01:44:55 +0300

    refactor(config): enhance next.config.ts with improved image handling, CORS headers, and build settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,49 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+  compress: true,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 7,
+  },
+  transpilePackages: ["lucide-react"],
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.API_BASE_URL}`,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
+  cleanDistDir: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
