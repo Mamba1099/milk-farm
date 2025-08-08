f6a1f7e2a83939752e7a0034bdc632f50e8a8843
Mamba1099 <mambakaranja240@gmail.com>
2025-08-09 02:58:39 +0300

    feat(employees): export CurrentUserProfileCard from index

--- components/employees/index.ts
+++ components/employees/index.ts
@@ -3,3 +3,4 @@ export { UserStatistics } from './user-statistics';
 export { UserTable } from './user-table';
 export { DesktopTableView } from './desktop-table-view';
 export { MobileCardView } from './mobile-card-view';
+export { CurrentUserProfileCard } from './current-user-profile-card';
