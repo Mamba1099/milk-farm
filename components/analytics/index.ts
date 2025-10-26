05c5ddd3b1b507c34cdae869d7a3810c4eb8a748
Mamba1099 <mambakaranja240@gmail.com>
2025-10-27 02:46:02 +0300

    feat(analytics): remove unused ProductionCalfFeedingChart and TopProducingCowsChart exports

--- components/analytics/index.ts
+++ components/analytics/index.ts
@@ -1,7 +1,5 @@
 export { DailyProductionChart } from "./daily-production-chart";
 export { SalesRevenueChart } from "./sales-revenue-chart";
-export { ProductionCalfFeedingChart } from "./production-calf-feeding-chart";
-export { TopProducingCowsChart } from "./top-producing-cows-chart";
 export { TreatmentCostTrendsChart } from "./treatment-cost-trends-chart";
 export { TreatmentExpenseChart } from "./treatment-expense-chart";
 export { ServingOutcomeChart } from "./serving-outcome-chart";
