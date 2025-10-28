d821c14af9559fc9d2f5b951fa82c802ed7fafd9
Mamba1099 <mambakaranja240@gmail.com>
2025-10-28 15:12:50 +0300

    refactor(next.config): remove unused headers configuration

--- next.config.ts
+++ next.config.ts
@@ -18,28 +18,6 @@ const nextConfig: NextConfig = {
   },
   transpilePackages: ["lucide-react"],
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: `${process.env.NEXT_API_URL}`,
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Content-Type, Accept",
-          },
-        ],
-      },
-    ];
-  },
   cleanDistDir: true,
   eslint: {
     ignoreDuringBuilds: true,
