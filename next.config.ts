b888e395af32fa79f74a32698a4cbde49992e6da
Mamba1099 <mambakaranja240@gmail.com>
2025-10-28 16:30:38 +0300

    refactor(next.config): remove custom headers configuration for API

--- next.config.ts
+++ next.config.ts
@@ -18,28 +18,6 @@ const nextConfig: NextConfig = {
   },
   transpilePackages: ["lucide-react"],
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://milk-farm-pink.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Content-Type, Accept, Authorization",
-          },
-        ],
-      },
-    ];
-  },
   cleanDistDir: true,
   eslint: {
     ignoreDuringBuilds: true,
