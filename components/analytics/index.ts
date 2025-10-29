7fa3b362240f425db7395958be723c8c40086c9e
Mamba1099 <mambakaranja240@gmail.com>
2025-10-29 05:09:38 +0300

    fix(analytics): correct export statement for DailyBalanceChart

--- components/analytics/index.ts
+++ components/analytics/index.ts
@@ -1,4 +1,5 @@
 export { DailyProductionChart } from "./daily-production-chart";
+export { default as DailyBalanceChart } from "./daily-balance-chart";
 export { SalesRevenueChart } from "./sales-revenue-chart";
 export { TreatmentCostTrendsChart } from "./treatment-cost-trends-chart";
 export { TreatmentExpenseChart } from "./treatment-expense-chart";
